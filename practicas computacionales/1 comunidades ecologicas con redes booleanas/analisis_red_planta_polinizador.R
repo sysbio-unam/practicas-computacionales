d98d1025cb1d3e56f9cc4853936e3ac77295f000
biologÍa de sistemas UNAM <79384818+sysbio-unam@users.noreply.github.com>
2021-03-16 19:17:05 -0600

    Add files via upload

---
+++ practicas computacionales/1 comunidades ecologicas con redes booleanas/analisis_red_planta_polinizador.R
@@ -0,0 +1,33 @@
+# Título: Comunidades ecologicas con redes booleanas
+
+# Nombre: Biología de sistemas
+
+# Fecha: Febreo 2021
+#######################################################################################
+
+# cargar librerías
+library(BoolNet)
+
+# establecer directorio de trabajo
+setwd("C:/Users/Elisa/Dropbox/Docencia adscripción biomédicas/EXAMENES Y RESPUESTAS A PRÁCTICAS PCBIOL 2021 1/ensamblaje de comunidades")
+
+# cargar red
+net <- loadNetwork("Red_planta_polinizador.txt")
+net
+
+# mostrar la red
+plotNetworkWiring(net)
+
+# obtener atractores
+attr <- getAttractors(net)
+attr
+
+# mostrar atractores
+plotAttractors(attr)
+plotStateGraph(attr) 
+
+# empezamos en (0,1,1,1,1) y preguntamos a qué atractor vamos a llegar
+path <- getPathToAttractor(net, c(0,1,1,1,1))
+path
+
+plotSequence(sequence=path)
